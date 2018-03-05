60939b6cce9da3f286dec97211c5ffb1b952a172
Matt Misbach <matt@misbach.org>
2018-03-05 15:16:52 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# fs-surnames
+A sample app that displays your closest deceased ancestors, and shows the surnames they come from.
+
+Click [here](https://misbach.github.io/fs-surnames/index.html) to see a running example.
+
+## To use
+Replace the "appKey" with your own appKey. To get an app key go [https://www.familysearch.org/developers/](https://www.familysearch.org/developers/)
+
+## Dependencies
+- jquery
+- [FamilySearch Javascript lite SDK](https://github.com/FamilySearch/fs-js-lite)
---
+++ logic.js
@@ -0,0 +1,67 @@
+let user = {};
+var fs = new FamilySearch({
+  environment: 'production',
+  appKey: 'a02j000000KTRjpAAH',
+  // redirectUri: 'https://misbach.github.io/fs-ancestors/'
+  redirectUri: 'http://localhost:5000/'
+});
+
+// Finish oauth flow by obtaining access_token
+fs.oauthResponse(function() {
+	$('.login').hide();
+
+	// Get current user's tree ID
+	fs.get('/platform/users/current', function(error, response) {
+		// Get User's ancestry
+		fs.get('/platform/tree/ancestry?generations=5&person='+response.data.users[0].personId, function(error, rsp) {
+			user.tree = rsp.data.persons;
+			deadHorizon(1);
+
+			// Display Horizon along with surnames
+			for (var i = 0; i < user.horizon.length; i++) {
+				let portrait = "https://api.familysearch.org/platform/tree/persons/"+user.horizon[i].pid+"/portrait?access_token="+fs.accessToken;
+				$('.tree').append('<li><a href="https://www.familysearch.org/tree/person/'+user.horizon[i].pid+'" target="_blank"><img class="portrait" src="'+portrait+'"><h3 class="ancestor">'+user.horizon[i].name+'</h3></a><br /><div class="'+user.horizon[i].pid+' surnames"></div></li>');
+
+				// Get surnames for each Horizon person
+				fs.get('/platform/tree/ancestry?generations=4&person='+user.horizon[i].pid, function(error, rsp) {
+					let surnames = {};
+					// Get surnames
+					for (var i = 0; i < rsp.data.persons.length; i++) {
+						if (rsp.data.persons[i].names[0].nameForms[0].parts[1]) {
+							let surname = rsp.data.persons[i].names[0].nameForms[0].parts[1].value;
+							if (surnames[surname] == undefined) surnames[surname] = rsp.data.persons[i].id;
+						}
+					}
+
+					// Display surnames
+					$.each(surnames, function(key, value) {
+					  $('.'+rsp.data.persons[0].id).append('<a href="https://www.familysearch.org/tree/person/'+value+'" target="_blank">'+key+"</a> ");
+					});
+				});
+			}
+		});
+	});
+});
+
+// Dead Horizon
+function deadHorizon(slot) {
+  // Create horizon object if doesn't exit
+  if (!user.horizon) user.horizon = [];
+  
+  // find node we're looking for in pedigree
+  for (var i=0; i<user.tree.length; i++) {
+    if (user.tree[i].display.ascendancyNumber == slot) {
+      if (user.tree[i].living == false) {
+        user.horizon.push(
+          { name: user.tree[i].display.name,
+            pid: user.tree[i].id,
+            slot: user.tree[i].display.ascendancyNumber
+          });
+      } else {
+        deadHorizon(slot * 2);
+        deadHorizon(slot * 2 +1);
+      }
+    }
+  }
+  return
+}		
